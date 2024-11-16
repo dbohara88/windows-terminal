d08a4940e19f164876fe06d6cf844e473beec048
Deepak Bohara <65617245+dbohara88@users.noreply.github.com>
2024-11-16 10:39:34 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,22 @@
-oh-my-posh init pwsh | Invoke-Expression
+#Aliases
+Set-Alias tt tree
+Set-Alias ll ls
+Set-Alias g git
+
+
+#Modules
+Import-Module -Name Terminal-Icons
+Import-Module z
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+
+#Prompt
+oh-my-posh init pwsh --config 'C:\Users\debug\Documents\PowerShell\myprofile.omp.json'| Invoke-Expression
+
+#Function
+function whereis ($command) {
+	Get-Command -Name $command -ErrorAction SilentlyContinue |
+	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
